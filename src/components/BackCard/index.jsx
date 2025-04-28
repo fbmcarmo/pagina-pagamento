6a80fc66d341b210b4cfa255ae128a11b3c5b966
fbmcarmo <fbrunomcarmo@gmail.com>
2025-04-28 11:58:07 -0300

    Novas alterações

---
+++ src/components/BackCard/index.jsx
@@ -0,0 +1,14 @@
+export default function BackCard(){
+    return(
+        <div className="w-[500px] h-[300px] bg-black rounded-xl">
+            <div className="w-full h-[30%] flex items-end">
+                <div className="w-full h-[40px] bg-[#2F2F2F]"></div>
+            </div>
+            <div className="w-full h-[70%] flex justify-center pt-[20px]">
+                <div className="w-[70%] h-[50px] bg-[#AEB6BF] flex items-center justify-end">
+                    <p className="text-black text-[20px] mr-2">000</p>
+                </div>
+            </div>
+        </div>
+    )
+}
